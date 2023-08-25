efc0e702d0aa1c831a13ea98bf563055750ee0e4
MARK OKAFOR <okaformark@gmail.com>
2023-08-25 15:51:26 -0500

    add signout

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "myamplifyapp410e74c3": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from './logo.svg';
+import {
+	Authenticator,
+	Button,
+	Heading,
+	Image,
+	View,
+	Card,
+} from '@aws-amplify/ui-react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import './App.css';
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+	return (
+		<Authenticator>
+			{({ signOut }) => (
+				<View className='App'>
+					<Card>
+						<Image src={logo} className='App-logo' alt='logo' />
+						<Heading level={1}>We now have Auth!</Heading>
+					</Card>
+					<Button onClick={signOut}>Sign Out</Button>
+				</View>
+			)}
+		</Authenticator>
+	);
 }
-
-export default App

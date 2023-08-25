47f1a4b63bd51a41672d361784de9263223fd9e7
MARK OKAFOR <okaformark@gmail.com>
2023-08-25 14:41:45 -0500

    amplify config for vite fix-3

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	resolve: {
-		alias: [
-			{
-				find: './runtimeConfig',
-				replacement: './runtimeConfig.browser', // ensures browser compatible version of AWS JS SDK is used
-			},
-		],
-	},
 });
